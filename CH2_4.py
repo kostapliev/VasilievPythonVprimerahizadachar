72a69a6b74aff83cf6f194701307da6257f9bb81
Konstantin Pliev <plievkd@mail.ru>
2022-01-20 12:05:51 +0300

    Update CH2_4.py

--- CH2_4.py
+++ CH2_4.py
@@ -1,5 +1,5 @@
 spisok1 = [1, 2, 4, 5]
-spisok2 = [1, 3, 4, 5]
+spisok2 = [1, 2, 4, 5]
 
 if len(spisok1) == len(spisok2):
     ind = 0
@@ -7,8 +7,10 @@ if len(spisok1) == len(spisok2):
         if i != spisok2[ind]:
             print("Не равны!")
             break
-        else:
-            print("Равны!")
+
+        if ind==len(spisok2)-1:
+            print("Равны")
             break
+        ind += 1
 else:
     print("Не равны")
