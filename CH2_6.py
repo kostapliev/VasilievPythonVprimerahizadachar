8c88d68ce1283c4fed324b517884ff9fec8e1542
Konstantin Pliev <plievkd@mail.ru>
2022-01-20 12:10:23 +0300

    Update CH2_6.py

--- CH2_6.py
+++ CH2_6.py
@@ -1,7 +1,6 @@
 dliny = eval(input("Введите длины трех сторон: "))
 
-if (dliny[0] <= (dliny[2] + dliny[1])) or (dliny[1] <= (dliny[0] + dliny[2])) or (dliny[2] <= (dliny[0] + dliny[1])):
-        print("Не существует")
+if (dliny[0] < (dliny[2] + dliny[1])) and (dliny[1] < (dliny[0] + dliny[2])) and (dliny[2] < (dliny[0] + dliny[1])):
+    print("существует")
 else:
-    print('существует')
-    
+    print('Не  существует')
